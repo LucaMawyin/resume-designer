007f201f86fba9c6b8f2a4e91cfd066e873661b1
LucaMawyin <lucamawyin@gmail.com>
2026-06-25 01:29:15 -0400

    removed alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -173,9 +173,6 @@ export default function Home() {
     // Submit info
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        alert(process.env.NEXT_PUBLIC_EC2_URL);
-
         const res = await fetch(`${process.env.NEXT_PUBLIC_EC2_URL}/api/route`, {
             method: "POST",
             headers: {
